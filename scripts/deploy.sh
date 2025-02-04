bdbd9a15649e21e5653fc46e897408e96e9f98e2
Shyam Prakash <catchshyam@gmail.com>
2025-02-04 22:38:52 +0530

    wip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,8 +12,8 @@ fi
 git pull origin main
 
 # Pull and recreate containers
-docker compose pull
-docker compose up -d --build
+docker-compose pull
+docker-compose up -d --build
 
 # Clean up unused images
 docker image prune -f
